03d3f61c3d59c645244c5d86b92b57b7d080a54a
Bence Nagy <nagybence@mailbox.unideb.hu>
2024-10-09 17:04:11 +0200

    Fix cors: include production frontend url from env

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.PRODUCTION_FRONTEND_URL],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
